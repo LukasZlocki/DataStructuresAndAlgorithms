1bca07877faa549b8200cf2bf84af31cdc8d313e
lzlocki <lzlocki@poczta.fm>
2022-11-11 11:01:48 +0100

    add one dimension array

---
+++ DataStructure/ArrayOneDimension.cs
@@ -0,0 +1,20 @@
+﻿namespace DataStructure;
+
+public class ArrayOneDimension
+{ 
+    string[] months = new string[12];
+    
+    public void CreateOneDomensionArray(){
+        
+        for (int month = 1; month < 12; month++ ){
+            DateTime monthName = new DateTime(2020, month, 1);
+            months[month] = monthName.ToString("MMMM");
+        }
+    }
+
+    public void PresentData(){
+        foreach(string element in months){
+            Console.WriteLine($"{element}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructure;
+
+
+
+
+// One Dimension Array - extracting names of months
+ArrayOneDimension arrayOneDimension = new ArrayOneDimension();
+arrayOneDimension.CreateOneDomensionArray();
+arrayOneDimension.PresentData();
