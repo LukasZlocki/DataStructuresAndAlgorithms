f7ac86d8098129e34c709b42d1320efdae329eee
lzlocki <lzlocki@poczta.fm>
2023-04-01 10:09:19 +0200

    Add reversing algo of hash table

--- Algorithms/Hash.cs
+++ Algorithms/Hash.cs
@@ -13,6 +13,15 @@ namespace Hash{
             hashtbl.Add("pko", "PKO");
         }
 
+        public void ReverseHashtable(){
+            Hashtable _reversedHT = new Hashtable();
+            foreach (DictionaryEntry element in hashtbl){
+                _reversedHT.Add(element.Value, element.Key);
+            }
+            hashtbl.Clear();
+            hashtbl = _reversedHT;
+        }
+
         public void ShowHashtable(){
             foreach(DictionaryEntry element in hashtbl) {
                 Console.WriteLine("Hash table | Key : {0} | Value : {1}", element.Key, element.Value);
