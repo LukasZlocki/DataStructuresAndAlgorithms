e6019f4a15a27dcba5e3e708bf409c47d783304c
lzlocki <lzlocki@poczta.fm>
2023-04-01 10:16:47 +0200

    Organize code in main program to see last task at the end off console

--- Program.cs
+++ Program.cs
@@ -2,6 +2,39 @@
 using DataStructuresAndAlgorithms.DataStructure;
 using Hash;
 
+// dpuble spac between lines
+void DoubleSpacer() {
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+
+// One dimension array - extracting names of months
+Console.WriteLine("One dimension array - extracting names of months");
+ArrayOneDimension arrayOneDimension = new ArrayOneDimension();
+arrayOneDimension.CreateOneDomensionArray();
+arrayOneDimension.PresentData();
+
+DoubleSpacer();
+
+// Multi dimension array - multiplication table
+Console.WriteLine("Multi dimension array - multiplication table");
+ArrayMultiDimension arrayMultiDimension = new ArrayMultiDimension();
+arrayMultiDimension.GenerateMultipleTable();
+arrayMultiDimension.ShowResultsOnScreen();
+
+DoubleSpacer();
+
+// selection sort 
+SelectionSorting ss2 = new SelectionSorting();
+Console.WriteLine("Table before sorting:");
+ss2.PrintTable();
+Console.WriteLine("Table after sorting");
+ss2.Selection();
+ss2.PrintTable();
+
+DoubleSpacer(); // space between the lines
+
 // Hashtable | reversing 
 HashReverse hashTbl = new HashReverse();
 hashTbl.SetHashtable();
@@ -12,40 +45,4 @@ Console.WriteLine("Reversing hash table, result :");
 hashTbl.ReverseHashtable();
 hashTbl.ShowHashtable();
 
-
-
-DoubleSpacer(); // space between the lines
-
-
-// selection sort 
-SelectionSorting ss2 = new SelectionSorting();
-Console.WriteLine("Table before sorting:");
-ss2.PrintTable();
-Console.WriteLine("Table after sorting");
-ss2.Selection();
-ss2.PrintTable();
-
-
-DoubleSpacer(); // space between the lines
-
-
-// One dimension array - extracting names of months
-Console.WriteLine("One dimension array - extracting names of months");
-ArrayOneDimension arrayOneDimension = new ArrayOneDimension();
-arrayOneDimension.CreateOneDomensionArray();
-arrayOneDimension.PresentData();
 DoubleSpacer();
-
-// Multi dimension array - multiplication table
-Console.WriteLine("Multi dimension array - multiplication table");
-ArrayMultiDimension arrayMultiDimension = new ArrayMultiDimension();
-arrayMultiDimension.GenerateMultipleTable();
-arrayMultiDimension.ShowResultsOnScreen();
-DoubleSpacer();
-
-
-
-void DoubleSpacer() {
-    Console.WriteLine();
-    Console.WriteLine();
-}
