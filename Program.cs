7f2ad8e3c3b37333e9b65ab2c202fbb928df462b
lzlocki <lzlocki@poczta.fm>
2023-04-01 10:12:45 +0200

    Add to main program hashtable class with all needed steps

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 ﻿using DataStructuresAndAlgorithms.Algorithms;
 using DataStructuresAndAlgorithms.DataStructure;
+using Hash;
+
+// Hashtable | reversing 
+HashReverse hashTbl = new HashReverse();
+hashTbl.SetHashtable();
+
+Console.WriteLine("Hash table before reversing:");
+hashTbl.ShowHashtable();
+Console.WriteLine("Reversing hash table, result :");
+hashTbl.ReverseHashtable();
+hashTbl.ShowHashtable();
 
 
+
+DoubleSpacer(); // space between the lines
+
+
+// selection sort 
 SelectionSorting ss2 = new SelectionSorting();
 Console.WriteLine("Table before sorting:");
 ss2.PrintTable();
 Console.WriteLine("Table after sorting");
 ss2.Selection();
 ss2.PrintTable();
+
 
 DoubleSpacer(); // space between the lines
 
